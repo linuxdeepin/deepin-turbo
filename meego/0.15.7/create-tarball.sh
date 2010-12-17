b70bb45028968c2ce1f021dfed06dede42d6b1d5
Nimika Keshri <nimika.1.keshri@nokia.com>
2010-12-17 12:59:13 +0200

    Changes: meego version 0.15.7 renamed to 0.15.10 RevBy: TrustMe

--- meego/0.15.7/create-tarball.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if test -n "$1"
-then
-    git archive --format tar --remote=$1 --output=meegotouch-applauncherd-0.15.7.tar --prefix meegotouch-applauncherd-0.15.7/ released/0.15.7
-    bzip2 meegotouch-applauncherd-0.15.7.tar
-else
-    echo "Usage: create-tarball.sh <repo>"
-    echo "       where <repo> is suitable for git archive --remote=<repo>"
-fi
