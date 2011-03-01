883e0097068b21df391c1137d00a1841a370dc01
Oskari Timperi <ext-oskari.timperi@nokia.com>
2011-03-01 16:25:26 +0200

    Changes: test-perf.rb modified to be more general
    Details: the script can be used to test the performance on both applauncherd
             and applifed. It can possibly be used for other apps too.
    
    RevBy: Olli Leppänen

--- tests/Harmattan/TestScripts/test-perf.rb
+++ tests/Harmattan/TestScripts/test-perf.rb
@@ -33,7 +33,7 @@ class TC_PerformanceTests < Test::Unit::TestCase
   FALA_GETTIME_BINARY = '/usr/bin/fala_gettime_ms'
   MATTI_LOCATION='/usr/lib/qt4/plugins/testability/libtestability.so'
   TEMPORARY_MATTI_LOCATION='/root/libtestability.so'
-   
+  
 
   @start_time = 0
   @end_time = 0
@@ -41,19 +41,19 @@ class TC_PerformanceTests < Test::Unit::TestCase
   @win_from_cache = 0
   @pos = 0
   @options = {}  
- 
+  
 
   $path = string = `echo $PATH `
   def print_debug(msg)
-      message = "[INFO]  #{msg}\n"
-      puts message
+    message = "[INFO]  #{msg}\n"
+    puts message
   end
 
   
   # method called before any test case
   def setup
 
-     optparse = OptionParser.new do|opts|
+    optparse = OptionParser.new do|opts|
       options = {}  
       # Set a banner, displayed at the top
       # of the help screen.
@@ -77,19 +77,19 @@ class TC_PerformanceTests < Test::Unit::TestCase
       options[:startup] = nil
       opts.on( '-s', '--startup MILLISECONDS', 'Time limit in milliseconds. Slower startup will make test to fail.' ) do|milliseconds|
         options[:startup] = milliseconds.to_i
-      print_debug("The Startup limit is : #{options[:startup]}")
+        print_debug("The Startup limit is : #{options[:startup]}")
       end
 
       options[:appCache] = nil
       opts.on( '-t', '--appCache MILLISECONDS', 'Time limit in milliseconds for application from cache.' ) do|milliseconds|
         options[:appCache] = milliseconds.to_i
-      print_debug("The limit for MApplication from cache is : #{options[:appCache]}")
+        print_debug("The limit for MApplication from cache is : #{options[:appCache]}")
       end
 
       options[:winCache] = nil
       opts.on( '-w', '--winCache MILLISECONDS', 'Time limit in milliseconds for window from cache.' ) do|milliseconds|
         options[:winCache] = milliseconds.to_i
-      print_debug("The limit for MApplicationWindow from cache is : #{options[:winCache]}")
+        print_debug("The limit for MApplicationWindow from cache is : #{options[:winCache]}")
       end
 
       options[:logFile] = nil
@@ -109,9 +109,7 @@ class TC_PerformanceTests < Test::Unit::TestCase
       @options=options
     end
 
-
     optparse.parse!
- 
 
     if @options[:application] == nil &&  @options[:command] == nil
       print_debug ("Application not defined!")
@@ -155,7 +153,7 @@ class TC_PerformanceTests < Test::Unit::TestCase
   end
   
 
-        
+  
   # method called after any test case for cleanup purposes
   def teardown
     print_debug ("exit from teardown")
@@ -169,136 +167,153 @@ class TC_PerformanceTests < Test::Unit::TestCase
     end
   end
   
+
+
   def open_Apps(appName)
-    
-    #Remove the Log file if it exists
+    # Remove the log files if they exist
     if FileTest.exists?(PIXELCHANGED_LOG)
       print_debug("remove #{PIXELCHANGED_LOG}")
       system "rm #{PIXELCHANGED_LOG}"
     end
+
     if FileTest.exists?(@options[:logFile])
       print_debug("remove #{@options[:logFile]}")
       system "rm #{@options[:logFile]}"
     end
+
     # Kill the binary if alive
     print_debug("Kill #{@options[:binary]}")
     system "pkill #{@options[:binary]}"
     sleep(2)
 
+    # execute the optional command if available
+    if @options[:pre_step] != nil 
+      print_debug ("pre_step: #{@options[:pre_step]}")
+      system "#{@options[:pre_step]}"
+    end
+
     if @options[:command] != nil
-      # execute the optional command if available
-      if @options[:pre_step] != nil 
-        print_debug ("pre_step: #{@options[:pre_step]}")
-        system "#{@options[:pre_step]}"
-      end
-
-
-      # Check the avarage system load is under 0.3
+      # Check that the average system load is under 0.3
       print_debug("Check the avarage system load is under 0.3")
       system "/usr/bin/waitloadavg.rb -l 0.3 -p 1.0 -t 100 -d"
 
       start_command ="`#{PIXELCHANGED_BINARY} -t 20x20 -t 840x466 -q >> #{PIXELCHANGED_LOG} &`; #{FALA_GETTIME_BINARY} \"Started from command line\" >>  #{PIXELCHANGED_LOG}; #{@options[:command]} &"
       print_debug ("start command: #{start_command}")
-      system "#{start_command}"
-      sleep (4)
-      print_debug ("pkill #{@options[:binary]}")
-      system "pkill \"#{@options[:binary]}\""
+      system start_command
 
+      sleep (4)
     else
-     # execute the optional command if available
-      if @options[:pre_step] != nil 
-        print_debug ("pre_step: #{@options[:pre_step]}")
-        system "#{@options[:pre_step]}"
-      end
-        
       @pos = `#{GET_COORDINATES_SCRIPT} -a #{@options[:application]}`
       @pos = @pos.split("\n")[-1]
       print_debug ("Co-ordinates: #{@pos}")
- 
+      
       print_debug("Check the avarage system load is under 0.3")
       system "/usr/bin/waitloadavg.rb -l 0.3 -p 1.0 -t 50 -d"
 
       cmd = "#{PIXELCHANGED_BINARY} -c #{@pos} -t 20x20 -t 840x466 -f #{PIXELCHANGED_LOG} -q"
       print_debug("pixel changed command is : #{cmd}")
       system cmd
+
       sleep (4)
+
       # Raise meegotouchhome to the top.
       #Workaround for keeping the window stack in shape.
       print_debug("#{GET_COORDINATES_SCRIPT} -g")
       system "#{GET_COORDINATES_SCRIPT} -g"
-
-      print_debug("pkill :#{@options[:binary]}")
-      system "pkill #{@options[:binary]}"
     end
 
+    print_debug("pkill :#{@options[:binary]}")
+    system "pkill #{@options[:binary]}"
   end
   
+
+
   def read_file(appName)
-    #Reading the log file to get the time
-    lines = File.open(PIXELCHANGED_LOG).readlines().collect { |x| x.split(" ")[0].to_i }
-    lines_app = File.open(@options[:logFile]).readlines().collect { |x| x.split(" ")[0].to_i }
-    #app_from_cache value
-    @app_from_cache = lines_app[1] - lines_app[0]   
-    print_debug ("App from cache #{@app_from_cache}")
+    def get_matching_lines(lines, re)
+      # return a list of lines that match re
+      lines.collect { |x| if x[1] =~ re; x; else; nil; end }.compact
+    end
 
-    @win_from_cache = lines_app[2] - lines_app[1]   
-    print_debug ("Window from cache #{@win_from_cache}")
+    # read times from pixelchanged log
+    lines = File.open(PIXELCHANGED_LOG).readlines().collect { |x| x.split()[0].to_i }
 
-    # First line tells when the button is released
+    # read stuff from application log, split lines to 2 parts
+    lines_app = File.open(@options[:logFile]).readlines().collect { |x| x.split(nil, 2) }
+
+    app_cache_lines = get_matching_lines(lines_app, /app from cache/)
+    win_cache_lines = get_matching_lines(lines_app, /win from cache/)
+
+    # check app cache time only if timeout specified
+    if @options[:appCache] != nil
+      assert(app_cache_lines.size > 0, "No 'app from cache' line found from logs!")
+
+      @app_from_cache = app_cache_lines[0][0].to_i - lines_app[0][0].to_i
+
+      print_debug("app from cache #{@app_from_cache}")
+    end
+
+    # check win cache time only if timeout specified (this needs the app cache line too)
+    if @options[:winCache] != nil
+      assert(app_cache_lines.size > 0, "no 'app from cache' line found from logs!")
+      assert(win_cache_lines.size > 0, "no 'win from cache' line found from logs!")
+
+      @win_from_cache = win_cache_lines[0][0].to_i - app_cache_lines[0][0].to_i
+
+      print_debug("win from cache #{@win_from_cache}")
+    end
+
+    # when the button was released
     @start_time = lines[0]
-    print_debug ("Started at : #{lines[0]}")
-    # Second one when the first pixel has changed its color
+
+    # when the first pixel was changed
     @end_time = lines[1]
-    print_debug ("Pixel changed: #{lines[1]}")
-    return @app_from_cache, @win_from_cache
+
+    print_debug("started at: #{@start_time}")
+    print_debug("pixel changed: #{@end_time}")
   end
   
   
-  def measure_time
-    #Measuring the Startup Time for applications
-    app_t = @end_time - @start_time
-    print_debug ("Startup time : #{app_t}")
-    return app_t
-  end
-  
+
   def test_performance
-    wL = []
-    list = []
-    wLsum = 0
+    start_time_sum = 0
     app_cache_sum = 0
     win_cache_sum = 0 
-    #Run Application with invoker
+
+    # run application and measure the times
     for i in 1..COUNT
       print_debug ("Now Launching  #{@options[:application]} #{i} times" )
+
       print_debug("Kill #{PIXELCHANGED_BINARY} if any before launching ")
       system("pkill #{PIXELCHANGED_BINARY}")
+
       open_Apps(@options[:application])
+
       sleep (5)
-      list.push(read_file(@options[:application]))
-      wL.push(measure_time)
+
+      read_file(@options[:application])
+      
+      if @options[:appCache] != nil
+        app_cache_sum += @app_from_cache
+      end
+
+      if @options[:winCache] != nil
+        win_cache_sum += @win_from_cache
+      end
+
+      start_time_sum += @end_time - @start_time
     end
-    
     
     print_debug ("Startup time in milliseconds\n")
     print_debug ("Application: #{@options[:application]} \n")
-    if @options[:startup] != nil
-      print_debug ("Time startup: #{@options[:startup]} ")
-    end 
     
-    #Printing the data
-    for i in 0..COUNT-1
-      print "%d \n" %[wL[i]]
-      wLsum = wLsum + wL[i]
-      app_cache_sum = app_cache_sum + list[i][0]
-      win_cache_sum = win_cache_sum + list[i][1]
-    end
-    print_debug ("Average Startup : #{wLsum/COUNT}")
-    print_debug ("MAppliacation from cache: #{app_cache_sum/COUNT}")
-    print_debug ("MApplicationWindow from cache #{win_cache_sum/COUNT}")
+    print_debug ("Average Startup : #{start_time_sum/COUNT}")
+
+    print_debug("MApplication from cache: #{app_cache_sum/COUNT}") if @options[:appCache] != nil
+    print_debug("MApplicationWindow from cache #{win_cache_sum/COUNT}") if @options[:winCache] != nil
 
     if @options[:startup] != nil
       print_debug("Check that startup time is less than #{@options[:startup]} ms")
-      assert((wLsum/COUNT) < @options[:startup], "Application: #{@options[:application]} avarage startup was slower than #{@options[:startup]} ms")
+      assert((start_time_sum/COUNT) < @options[:startup], "Application: #{@options[:application]} avarage startup was slower than #{@options[:startup]} ms")
     end
 
     if @options[:appCache] != nil
