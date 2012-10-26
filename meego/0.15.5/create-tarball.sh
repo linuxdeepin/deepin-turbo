1c4c1fbfcf47f8ba8232a55c37eea860c7c1688b
Robin Burchell <robin+git@viroteck.net>
2012-10-26 12:44:04 +0200

    remove meego/ directory

--- meego/0.15.5/create-tarball.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if test -n "$1"
-then
-    git archive --format tar --remote=$1 --output=meegotouch-applauncherd-0.15.5.tar --prefix meegotouch-applauncherd-0.15.5/ released/0.15.5
-    bzip2 meegotouch-applauncherd-0.15.5.tar
-else
-    echo "Usage: create-tarball.sh <repo>"
-    echo "       where <repo> is suitable for git archive --remote=<repo>"
-fi
