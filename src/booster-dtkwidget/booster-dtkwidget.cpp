58abe17360380d99b94f6dfcc2154b4a6c963e06
zccrs <ccrr1314@live.com>
2018-08-30 14:56:35 +0800

    feat: support dtk widget
    Change-Id: I97550a7c028e70e0cb477b914a98c03157cabe9b

---
+++ src/booster-dtkwidget/booster-dtkwidget.cpp
@@ -0,0 +1,67 @@
+/***************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (directui@nokia.com)
+**
+** This file is part of applauncherd
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at directui@nokia.com.
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
+
+#include "booster-dtkwidget.h"
+#include "daemon.h"
+
+#include <QApplication>
+#include <QWidget>
+
+const string QWBooster::m_boosterType  = "dtkwidget";
+
+const string & QWBooster::boosterType() const
+{
+    return m_boosterType;
+}
+
+void QWBooster::initialize(int initialArgc, char **initialArgv, int boosterLauncherSocket,
+                           int socketFd, SingleInstance *singleInstance, bool bootMode)
+{
+    qputenv("DXCB_FAKE_PLATFORM_NAME_XCB", "true");
+    qputenv("QT_QPA_PLATFORM", "dxcb");
+
+    static int argc = initialArgc;
+    Q_UNUSED(new QApplication(argc, initialArgv));
+
+    Booster::initialize(initialArgc, initialArgv, boosterLauncherSocket, socketFd, singleInstance, bootMode);
+}
+
+bool QWBooster::preload()
+{
+    // 初始化QWidget，减少程序启动后第一次显示QWidget时的时间占用
+    QWidget widget;
+
+    widget.setWindowFlags(Qt::BypassWindowManagerHint
+                          | Qt::WindowStaysOnBottomHint
+                          | Qt::WindowTransparentForInput
+                          | Qt::WindowDoesNotAcceptFocus);
+    widget.setFixedSize(1, 1);
+    widget.show();
+
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    QWBooster *booster = new QWBooster;
+
+    Daemon d(argc, argv);
+    d.run(booster);
+}
+
