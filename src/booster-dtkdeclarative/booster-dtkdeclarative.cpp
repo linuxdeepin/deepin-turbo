31c9359c5f15c9305b0186a044d882f639be0aa9
zccrs <zccrs@live.com>
2021-01-07 09:54:34 +0800

    feat: 为dtkqml类型的程序添加preload
    在preload中尝试创建QQuickView, 并尝试加载一个带控件的
    qml文件, 以此尽可能的引入更多必用的动态库, 加速程序的
    启动过程.
    
    Log:
    Change-Id: Ie495a58e888c72793263dd728df01fdf5dc02000

--- src/booster-dtkdeclarative/booster-dtkdeclarative.cpp
+++ src/booster-dtkdeclarative/booster-dtkdeclarative.cpp
@@ -26,6 +26,10 @@
 #include "booster-dtkdeclarative.h"
 
 #include <DAppLoader>
+#include <DWindowManagerHelper>
+#include <QImageReader>
+#include <QQmlComponent>
+#include <QQuickView>
 
 DQUICK_USE_NAMESPACE
 
@@ -67,6 +71,20 @@ int DeclarativeBooster::launchProcess()
 
 bool DeclarativeBooster::preload()
 {
+    // 初始化一些必要的数据, 为后面加载的正常程序组好缓存
+    QQuickView window;
+    QQmlComponent component(window.engine());
+    component.setData("import QtQuick.Controls 2.4\nButton { }", QUrl());
+    component.create(window.rootContext());
+    window.create();
+
+    // 当创建窗口时会初始化窗口的class name, 但是此时还未加载实际的程序, 缓存的class name
+    // 无用, 因此要清理缓存的数据, 避免加载程序后的正常窗口的wm class name错误
+    DTK_GUI_NAMESPACE::DWindowManagerHelper::setWmClassName(QByteArray());
+
+    // 初始化图片解码插件，在龙芯和申威上，Qt程序冷加载图片解码插件几乎耗时1s
+    Q_UNUSED(QImageReader::supportedImageFormats());
+
     return true;
 }
 
