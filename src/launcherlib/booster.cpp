2f3591d490d520fb9af8e7c6f9d83ead5fd893f3
AlexOne <wangpenga@uniontech.com>
2021-11-12 15:13:51 +0800

    chore: 修改 LGPL 为 GPL
    修改 LGPL 为 GPL
    因为和依赖不兼容，所以修改协议
    
    Log:
    Influence: 软件授权协议
    Change-Id: I07d407907290b5ad2f9552cb9dfb8e35f6967b40

--- src/launcherlib/booster.cpp
+++ src/launcherlib/booster.cpp
@@ -10,8 +10,8 @@
 ** Nokia at directui@nokia.com.
 **
 ** This library is free software; you can redistribute it and/or
-** modify it under the terms of the GNU Lesser General Public
-** License version 2.1 as published by the Free Software Foundation
+** modify it under the terms of the GNU General Public
+** License version 2 or later (the "GPL") as published by the Free Software Foundation
 ** and appearing in the file LICENSE.LGPL included in the packaging
 ** of this file.
 **
